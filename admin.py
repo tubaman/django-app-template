40280ee60510a9bd2c95dd7af8f36c30c8e5e9d3
Ryan Nowakowski <tubaman@fattuba.com>
2013-01-26 12:07:56 -0600

    add admin by default

---
+++ admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# expose your models like this:
+# from models import MyModel
+# class MyModeModeln(admin.ModelAdmin):
+#    pass
+# 
+# admin.site.register(MyModel, MyModelAdmin)
